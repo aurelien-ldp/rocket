fa4012c873955acd291603b18a3e01671137980d
Aurelien LDP <aurelien.ldp@gmail.com>
2017-06-15 15:54:58 +0200

    Documentation about responding in YAML and JSON

--- examples/basic.cr
+++ examples/basic.cr
@@ -1,9 +1,32 @@
 require "../src/rocket"
+require "json"
+require "yaml"
 
 # Define your controllers and actions as you wish
 class HelloController < Rocket::Controller
   def get
-    "GET !"
+    user = { username: "aurel", password: "123456" }
+    user.to_json
+  end
+
+  def index_json
+    posts = [
+      { title: "Hello 0", content: "This is the content 0" },
+      { title: "Hello 1", content: "This is the content 1" },
+      { title: "Hello 2", content: "This is the content 2" },
+      { title: "Hello 3", content: "This is the content 3" },
+    ]
+    posts.to_json
+  end
+
+  def index_yaml
+    posts = [
+      { title: "Hello 0", content: "This is the content 0" },
+      { title: "Hello 1", content: "This is the content 1" },
+      { title: "Hello 2", content: "This is the content 2" },
+      { title: "Hello 3", content: "This is the content 3" },
+    ]
+    posts.to_yaml
   end
 
   def post
@@ -31,6 +54,8 @@ end
 
 # Add routes to the router
 Rocket::ROUTER.add_route("GET", "/", HelloController, "get")
+Rocket::ROUTER.add_route("GET", "/posts.json", HelloController, "index_json")
+Rocket::ROUTER.add_route("GET", "/posts.yaml", HelloController, "index_yaml")
 Rocket::ROUTER.add_route("POST", "/posts", HelloController, "post")
 Rocket::ROUTER.add_route("PUT", "/", HelloController, "put")
 Rocket::ROUTER.add_route("PATCH", "/", HelloController, "patch")
